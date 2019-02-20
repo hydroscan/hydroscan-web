6e8a309dfbde1c3e5d59d6981eca197e6aded7b0
wlchn <wlchn@qq.com>
2019-02-20 17:17:01 +0800

    responsive layouts

--- src/common/layouts/Document.tsx
+++ src/common/layouts/Document.tsx
@@ -17,7 +17,7 @@ class Document extends React.Component<DocumentProps & DocumentExtraProps> {
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
           <meta charSet="utf-8" />
           <title>Hydroscan</title>
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
           {assets.client.css ? <link rel="stylesheet" href={assets.client.css} /> : ''}
           <style type="text/css" id="server-side-styles">
             {styleSheets.toString()}
