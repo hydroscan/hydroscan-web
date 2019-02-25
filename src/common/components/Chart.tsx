601d71c2a935d40ffc0e37fac350854976955dab
wlchn <wlchn@qq.com>
2019-02-25 11:06:02 +0800

    display volume chart

--- src/common/components/Chart.tsx
+++ src/common/components/Chart.tsx
@@ -3,51 +3,7 @@ import { connect } from 'react-redux';
 import './Chart.scss';
 import { fetchTradesChart } from '../actions/trade';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
-
-const testData = [
-  {
-    name: 'Page A',
-    uv: 4000,
-    pv: 2400,
-    amt: 2400
-  },
-  {
-    name: 'Page B',
-    uv: 3000,
-    pv: 1398,
-    amt: 2210
-  },
-  {
-    name: 'Page C',
-    uv: 2000,
-    pv: 9800,
-    amt: 2290
-  },
-  {
-    name: 'Page D',
-    uv: 2780,
-    pv: 3908,
-    amt: 2000
-  },
-  {
-    name: 'Page E',
-    uv: 1890,
-    pv: 4800,
-    amt: 2181
-  },
-  {
-    name: 'Page F',
-    uv: 2390,
-    pv: 3800,
-    amt: 2500
-  },
-  {
-    name: 'Page G',
-    uv: 3490,
-    pv: 4300,
-    amt: 2100
-  }
-];
+// import { formatVolumeUsd } from '../lib/formatter';
 
 const mapStateToProps = state => {
   return {
@@ -63,6 +19,10 @@ class Chart extends React.PureComponent<any, any> {
 
   public render() {
     const { chartData } = this.props;
+    if (chartData.length === 0) {
+      return <div />;
+    }
+
     return (
       <div className="Chart">
         <LineChart
@@ -77,11 +37,12 @@ class Chart extends React.PureComponent<any, any> {
           }}>
           <CartesianGrid strokeDasharray="3 3" />
           <XAxis dataKey="date" />
-          <YAxis />
+          <YAxis yAxisId="volume" />
+          <YAxis yAxisId="trades" orientation="right" />
           <Tooltip />
           <Legend />
-          <Line type="monotone" dataKey="volume" stroke="#8884d8" activeDot={{ r: 8 }} />
-          <Line type="monotone" dataKey="trades" stroke="#82ca9d" />
+          <Line type="monotone" dataKey="volume" yAxisId="volume" stroke="#8884d8" />
+          <Line type="monotone" dataKey="trades" yAxisId="trades" stroke="#82ca9d" />
         </LineChart>
       </div>
     );
