9cb3beac801547d310ef5891bff17cec81cab5e8
wlchn <wlchn@qq.com>
2019-03-08 18:00:21 +0800

    page 404

--- src/common/layouts/NotFound.tsx
+++ src/common/layouts/NotFound.tsx
@@ -1,8 +1,29 @@
 import React from 'react';
+import { Link } from 'found';
+import './NotFound.scss';
 
 class NotFound extends React.Component {
   public render() {
-    return <div>Not Found</div>;
+    return (
+      <div className="NotFound">
+        <div className="item-wrapper">
+          <img className="not-found-left" src={require('../images/404-left.png')} />
+          <img className="hydroscan-gray" src={require('../images/hydroscan-gray.png')} />
+          <img className="not-found-right" src={require('../images/404-right.png')} />
+        </div>
+        <div className="item-wrapper">
+          <img className="not-found-text" src={require('../images/404.png')} />
+        </div>
+        <div className="item-wrapper">
+          <div className="sorry">Sorry. The page you are looking for does not exist or has been removed.</div>
+        </div>
+        <div className="item-wrapper">
+          <Link className="link" to="/">
+            Go to Home Page
+          </Link>
+        </div>
+      </div>
+    );
   }
 }
 
