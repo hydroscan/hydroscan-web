9b2cbae4c0b3466b687bac78c70c9ff1c7fea7ee
wlchn <wlchn@qq.com>
2019-03-11 11:08:27 +0800

    add copy address to footer

--- src/common/components/Footer.tsx
+++ src/common/components/Footer.tsx
@@ -1,12 +1,30 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import './Footer.scss';
+import copy from 'clipboard-copy';
+import Toastify from 'toastify-js';
 
 const mapStateToProps = state => {
   return {};
 };
 
 class Footer extends React.PureComponent<any, any> {
+  private donateAddress = '0xE3Cba0C3ce94365D3E59C7bA6352D598DBCc00b1';
+
+  constructor(props) {
+    super(props);
+  }
+
+  public handleCopy() {
+    copy(this.donateAddress);
+    Toastify({
+      text: 'Copied successfully!',
+      duration: 3000,
+      gravity: 'bottom',
+      backgroundColor: '#6872e0'
+    }).showToast();
+  }
+
   public render() {
     return (
       <div className="Footer">
@@ -14,14 +32,20 @@ class Footer extends React.PureComponent<any, any> {
           <div className="content-wrapper">
             <div className="donate">Donate to Help us Improve</div>
             <div className="copy-wrapper">
-              <div className="address">0xE3Cba0C3ce94365D3E59C7bA6352D598DBCc00b1</div>
-              <div className="copy gradient">Copy Address</div>
+              <div className="address">{this.donateAddress}</div>
+              <div
+                className="copy gradient"
+                onClick={() => {
+                  this.handleCopy();
+                }}>
+                Copy Address
+              </div>
             </div>
             <div className="link-wrapper">
               <a href="https://github.com/hydroscan" target="_black">
                 <img className="github" src={require('../images/github.svg')} />
               </a>
-              <a href="https://etherscan.io/address/0xE3Cba0C3ce94365D3E59C7bA6352D598DBCc00b1" target="_black">
+              <a href={`https://etherscan.io/address/${this.donateAddress}`} target="_black">
                 <img className="etherscan" src={require('../images/etherscan.png')} />
               </a>
             </div>
