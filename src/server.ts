0b3fe50471a8a35ab34408b470d566aa0a9d248d
wlchn <wlchn@qq.com>
2019-07-17 21:53:41 +0800

    Protection policy

--- src/server.ts
+++ src/server.ts
@@ -29,7 +29,22 @@ server.get('/ping', (req, res) => res.send('pong'));
 server
   .disable('x-powered-by')
   .use(express.static(process.env.RAZZLE_PUBLIC_DIR || ''))
-  .get('/*', serverConfig);
+  .get('/*', (req, res) => {
+    if (
+      req.path.includes('%') ||
+      req.path.includes('script') ||
+      req.path.includes('.js') ||
+      req.path.includes('.json')
+    ) {
+      res.redirect('/404');
+    }
+    res.append('X-XSS-Protection', '1; mode=block');
+    res.append('X-Frame-Options', 'DENY');
+    res.append('Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
+    res.append('Referrer-Policy', 'strict-origin-when-cross-origin');
+    res.append('X-Content-Type-Options', 'nosniff');
+    serverConfig(req, res);
+  });
 
 server.use((err, req, res, next) => {
   console.error(err.stack);
